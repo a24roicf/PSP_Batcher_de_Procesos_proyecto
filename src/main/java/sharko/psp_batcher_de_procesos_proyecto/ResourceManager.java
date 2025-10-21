f3e13591743b4d7ae1cfee59247fd7f026d9732a
Sharkoise <a24roicf@iesantonlosada.gal>
2025-10-21 16:59:47 +0200

    Implementacion metodos Resource Manager
    todos los metodos esenciales para el funcionamiento de los jobs si pueden o no ejecutarse

--- src/main/java/sharko/psp_batcher_de_procesos_proyecto/ResourceManager.java
+++ src/main/java/sharko/psp_batcher_de_procesos_proyecto/ResourceManager.java
@@ -17,4 +17,32 @@ public class ResourceManager {
         this.availableCpuCores = totalCpuCores;                 //cpu cores disponible
         this.availableMemMb = totalMemMb;                       //memoria disponible
     }
+    
+    //Booleano de si puede iniciar el job o no
+    public boolean canRun(Job job) {
+        return job.getCpuCores() <= availableCpuCores &&
+               job.getMemMb() <= availableMemMb;
+    }
+
+    //Reserva recursos cuando un job inicia
+    public void allocate(Job job) {
+        availableCpuCores -= job.getCpuCores();
+        availableMemMb -= job.getMemMb();
+        System.out.printf("Recursos asignados a %s → CPU: %d, MEM: %dMB%n",
+                job.getName(), job.getCpuCores(), job.getMemMb());
+    }
+
+    //Libera recursos cuando un job termina o falla
+    public void release(Job job) {
+        availableCpuCores += job.getCpuCores();
+        availableMemMb += job.getMemMb();
+        System.out.printf("Recursos liberados por %s → CPU: %d, MEM: %dMB%n",
+                job.getName(), job.getCpuCores(), job.getMemMb());
+    }
+
+    //Estado
+    public void printStatus() {
+        System.out.printf("Recursos disponibles → CPU: %d/%d, MEM: %d/%d MB%n",
+                availableCpuCores, totalCpuCores, availableMemMb, totalMemMb);
+    }
 }
